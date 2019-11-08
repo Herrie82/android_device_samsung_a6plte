9c7ebea1d3abfbf765b0280a70643279cad21177
Junak <junak.pub@gmail.com>
2019-11-08 14:32:54 +0200

    add functions to fix cellular signal indication to shim
    Change-Id: I206a8de14b02dc93e0bf22600b013b716b27a171

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -4,6 +4,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_SHARED_LIBRARIES := libdl
 LOCAL_VENDOR_MODULE := true
 LOCAL_SRC_FILES:= secril_shim.cpp
 LOCAL_MODULE := libshim_secril
--- libshims/secril_shim.cpp
+++ libshims/secril_shim.cpp
@@ -1,5 +1,38 @@
+#include <dlfcn.h>
+#include <assert.h>
+
 extern "C" {
-    extern int _ZN14TrafficControl15connectToServerEb(bool) {
-        return -1;
-    };
+
+typedef int (*resp_t)(int,int,int,int,void*,size_t);
+
+extern int _ZN5radio24currentSignalStrengthIndEiii9RIL_ErrnoPvj(
+        int slotId, int indicationType, int token, int e, void *response, size_t responseLen)
+{
+    static resp_t orig = 0;
+    if (!orig)
+        orig = (resp_t) dlsym(RTLD_NEXT, __func__);
+    assert(orig != NULL);
+    if (responseLen == 0x4c)
+        return orig(slotId, indicationType, token, e, response, 56);
+    else
+        return orig(slotId, indicationType, token, e, response, responseLen);
+}
+
+extern int _ZN5radio25getSignalStrengthResponseEiii9RIL_ErrnoPvj(
+        int slotId, int responseType, int serial, int e, void *response, size_t responseLen)
+{
+    static resp_t orig = 0;
+    if (!orig)
+        orig = (resp_t) dlsym(RTLD_NEXT, __func__);
+    assert(orig != NULL);
+    if (responseLen == 0x4c)
+        return orig(slotId, responseType, serial, e, response, 56);
+    else
+        return orig(slotId, responseType, serial, e, response, responseLen);
+}
+
+extern int _ZN14TrafficControl15connectToServerEb(bool) {
+    return -1;
+}
+
 }
